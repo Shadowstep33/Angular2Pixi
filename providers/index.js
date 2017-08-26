cd561722e562c9a2048ee3c0f5d5c888f111e267
Shadowstep33 <gabriel@squarevault.com>
2017-08-26 12:15:33 -0500

    build

--- providers/index.js
+++ providers/index.js
@@ -3,5 +3,6 @@ function __export(m) {
     for (var p in m) if (!exports.hasOwnProperty(p)) exports[p] = m[p];
 }
 Object.defineProperty(exports, "__esModule", { value: true });
-__export(require("./pixi.module"));
-__export(require("./providers"));
+__export(require("./asset.service"));
+__export(require("./scene.service"));
+__export(require("./pixi.service"));
