bdcbc0f40c302903a368140005339643679b7548
Shadowstep33 <gabriel@squarevault.com>
2018-11-24 12:44:18 -0600

    menu supports row length now; filter stuff

--- src/components/filters/filters.directive.ts
+++ src/components/filters/filters.directive.ts
@@ -1,51 +1,47 @@
-import {
-  Directive,
-  Input,
-  SimpleChanges,
-} from '@angular/core';
-import * as PIXI from 'pixi.js';
-import * as filters from 'pixi-filters';
+import { Directive, Input, SimpleChanges } from "@angular/core";
+import * as PIXI from "pixi.js";
+import * as filters from "pixi-filters";
 
 @Directive({
-  selector: '[a2pfilter]',
+  selector: "[a2pfilter]"
 })
 export class FilterDirective {
-
-  @Input('container') container;
-  @Input('a2pfilter')
+  @Input("container")
+  container;
+  @Input("a2pfilter")
   set filterConfig(value) {
-    console.log("Tryina change", value, this._filterConfig, this.filter)
+    console.log("Tryina change", value, this._filterConfig, this.filter);
     if ((this.filter ? this.filter.type : true) != value.type) {
-      console.log("fitle rchanger", value)
+      console.log("fitle rchanger", value);
       this._filterConfig = value;
 
       if (filters[this._filterConfig.type]) {
         this.wipeFilter();
         this.initFilter();
       } else {
-        console.log(this._filterConfig.type + " filter not available (from set)", filters)
+        console.log(
+          this._filterConfig.type + " filter not available (from set)",
+          filters
+        );
       }
     }
   }
 
   _filterConfig = {
     type: "BlurFilter"
-  }
+  };
 
   filter: any;
 
-  constructor(
-  ) {
-  }
+  constructor() {}
 
   ngOnInit() {
-    if (this.container === null)
-      return;
+    if (this.container === null) return;
 
     if (filters[this._filterConfig.type]) {
       this.initFilter();
     } else {
-      console.log(this._filterConfig.type + " filter not available", filters)
+      console.log(this._filterConfig.type + " filter not available", filters);
     }
   }
 
@@ -57,15 +53,12 @@ export class FilterDirective {
   }
 
   applyConfig() {
-    for (let c in this._filterConfig)
-      this.filter[c] = this._filterConfig[c];
+    for (let c in this._filterConfig) this.filter[c] = this._filterConfig[c];
   }
 
   applyFilter() {
-    if (this.container.filters)
-      this.container.filters.push(this.filter);
-    else
-      this.container.filters = [this.filter];
+    if (this.container.filters) this.container.filters.push(this.filter);
+    else this.container.filters = [this.filter];
   }
 
   wipeFilter() {
